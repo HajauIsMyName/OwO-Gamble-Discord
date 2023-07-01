00449dd5578edc25e32b9ba912edca200875a6de
HajauIsMyName <hieuhau4@gmail.com>
2023-07-01 15:20:04 +0700

    update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import aiohttp
 
 from logging.config import dictConfig
 from datetime import datetime
-from discord.ext import commands
 
 
 class CustomFormatter(logging.Formatter):
@@ -37,88 +36,91 @@ class CustomFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt, "%d %b %Y %H:%M:%S")
         return formatter.format(record)
 
-    def loggerStatus(self, amount=0):
-        if amount >= 0:
+    def loggerStatus(self, status: str = None, amount: int = 0):
+        if status.lower() == "won":
             print(
                 f"\x1b[43m{datetime.today().strftime('%d %b %Y %H:%M:%S')}\x1b[0m  - {self.GREEN}WON{self.RESET} - {amount:,} cowoncy")
 
-        else:
+        elif status.lower() == "lost":
             print(
                 f"\x1b[43m{datetime.today().strftime('%d %b %Y %H:%M:%S')}\x1b[0m  - {self.RED}LOST{self.RESET} - {abs(amount):,} cowoncy")
 
-    def loggerCash(self, cash=0):
-        print(f"\x1b[43m{datetime.today().strftime('%d %b %Y %H:%M:%S')}\x1b[0m  - {self.CYAN}CASH{self.RESET} - You currently have {cash:,} cowoncy")
+        elif status.lower() == "cash":
+            print(
+                f"\x1b[43m{datetime.today().strftime('%d %b %Y %H:%M:%S')}\x1b[0m  - {self.CYAN}CASH{self.RESET} - You currently have {amount:,} cowoncy")
 
 
-def returnJson(var=""):
-    with open("conf.json", "r") as file:
-        data = json.load(file)
-        return data[var]
+class Data:
+    def __init__(self):
+        with open("conf.json", "r") as file:
+            self.data = json.load(file)
+            self.token = str(self.data["token"])
+            self.channel = int(self.data["channel"])
+            self.bet = int(self.data["bet"])
+            self.webhook = self.data["webhook"]
 
 
-client = commands.Bot(command_prefix="hajauneverdie", help_command=None)
-cmds = random.choice(["heads", "tails"])
-bet = returnJson("bet")
+class Client(discord.Client, Data):
+    def __init__(self, *args, **kwargs):
+        discord.Client.__init__(self, *args, **kwargs)
+        Data.__init__(self)
 
+        self.total_win = 0
+        self.total_lost = 0
+        self.botID = 408785106942164992
+        self.cmds = random.choice(["heads", "tails"])
 
-@client.event
-async def on_ready():
-    channel = client.get_channel(returnJson("channel"))
-    bet = returnJson("bet")
+    async def on_ready(self):
+        channel = self.get_channel(self.channel)
 
-    os.system("cls" if os.name == "nt" else "clear")
-    logger.info(f"Logged in as {client.user} (ID: {client.user.id})")
-    print("------" * 5)
+        os.system("cls" if os.name == "nt" else "clear")
+        logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------" * 5)
 
-    while True:
-        await channel.send(f"owocf {bet} {cmds}")
-        logger.info(f"Sent {bet:,} cowoncy to {cmds}")
-        await asyncio.sleep(7)
+        while True:
+            await channel.send(f"owocf {self.bet} {self.cmds}")
+            logger.info(f"Sent {self.bet:,} cowoncy to {self.cmds}")
+            await asyncio.sleep(7)
 
-        await channel.send("owocash")
-        logger.info("Checked cash")
-        await asyncio.sleep(10)
+            await channel.send("owocash")
+            logger.info("Checked cash")
+            await asyncio.sleep(10)
 
+    async def on_message(self, message):
+        Format = CustomFormatter()
+        await asyncio.sleep(5)
 
-@client.event
-async def on_message(message):
-    global cmds, bet
+        if message.author == self.get_user(self.botID):
+            if "⚠" in message.content:
+                if not self.webhook["link"] == "":
+                    async with aiohttp.ClientSession() as session:
+                        webhook = discord.Webhook.from_url(
+                            self.webhook["link"], adapter=discord.AsyncWebhookAdapter(session))
+                        await webhook.send(f"<@{self.webhook['ping']}> Verification Found!")
 
-    await asyncio.sleep(5)
+                logger.critical("Detected Verification!")
+                await self.close()
 
-    if message.author == client.get_user(408785106942164992):
-        if "⚠" in message.content:
-            link = returnJson("webhook")["link"]
-            ping = returnJson("webhook")["ping"]
+            elif "and you won" in message.content:
+                Format.loggerStatus("won", self.bet)
+                self.bet = self.bet
 
-            if link:
-                async with aiohttp.ClientSession() as session:
-                    webhook = discord.Webhook.from_url(
-                        link, adapter=discord.AsyncWebhookAdapter(session))
-                    await webhook.send(f"<@{ping}> Verification Found!")
+            elif "and you lost it all..." in message.content:
+                Format.loggerStatus("lost", -self.bet)
 
-            logger.critical("Detected Verification!")
-            await client.close()
+                self.bet *= 2
+                self.cmds = "tails" if self.cmds == "heads" else "heads"
 
-        elif "and you won" in message.content:
-            CustomFormatter().loggerStatus(bet)
-            bet = returnJson("bet")
+            elif "currently" in message.content:
+                currentCash = "".join(re.findall(
+                    "[0-9]+", message.content[message.content.find("have")::]))
+                Format.loggerStatus("cash", int(currentCash))
 
-        elif "and you lost it all..." in message.content:
-            CustomFormatter().loggerStatus(-bet)
+        elif message.author == self.user:
+            if message.content == "stop":
+                logger.info("Stopped")
+                await self.close()
 
-            bet = returnJson("bet") if bet > 128000 else bet * 2
-            cmds = "tails" if cmds == "heads" else "heads"
-
-        elif "currently" in message.content:
-            currentCash = "".join(re.findall(
-                "[0-9]+", message.content[message.content.find("have")::]))
-            CustomFormatter().loggerCash(int(currentCash))
-
-    elif message.author == client.user:
-        if message.content == "stop":
-            logger.info("Stopped")
-            await client.close()
 
 dictConfig({
     "version": 1,
@@ -133,9 +135,11 @@ logger.addHandler(ch)
 
 
 if __name__ == "__main__":
+    client = Client(
+        guild_subscription_options=discord.GuildSubscriptionOptions.off())
+
     try:
-        token = returnJson("token")
-        client.run(token)
+        client.run(client.token)
 
     except:
         pass
